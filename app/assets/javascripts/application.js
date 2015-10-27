dc9dd15aa161b11e1eb3ac18b8b395ebd7df8e1b
VishwasN <vishwasn.pis@gmail.com>
2015-10-27 16:35:26 +0530

    Added controller and service

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,45 @@
 //= require bootstrap.min
 //= require suggest.min
 //= require controllers/app
+//= require controllers/record
+//= require controllers/users
+//= require services/recordService
+//= require services/sessionService
+
+
+angular.module('angularapp', ['sessionService','recordService','$strap.directives'])
+  .config(['$httpProvider', function($httpProvider){
+        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+
+        var interceptor = ['$location', '$rootScope', '$q', function($location, $rootScope, $q) {
+            function success(response) {
+                return response
+            };
+
+            function error(response) {
+                if (response.status == 401) {
+                    $rootScope.$broadcast('event:unauthorized');
+                    $location.path('/users/login');
+                    return response;
+                };
+                return $q.reject(response);
+            };
+
+            return function(promise) {
+                return promise.then(success, error);
+            };
+        }];
+        $httpProvider.responseInterceptors.push(interceptor);
+  }])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/', {templateUrl:'/home/index.html'})
+      .when('/record', {templateUrl:'/record/index.html', controller:RecordCtrl})
+      .when('/users/login', {templateUrl:'/users/login.html', controller:UsersCtrl})
+      .when('/users/register', {templateUrl:'/users/register.html', controller:UsersCtrl});
+  }]);
+
+
 
 
 
